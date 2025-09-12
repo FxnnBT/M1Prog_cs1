24cd8385c0e3537c166af9875344465ab9cc638f
FxnnBT <40134@ma-web.nl>
2025-09-12 13:11:06 +0200

    m1prog 2nd initial commit

---
+++ 00_install_en_project/verhaal/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace verhaal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Control is sometimes an illusion, but sometimes you need illusion to gain control. Control is only real if accept it");
+    }
+}
