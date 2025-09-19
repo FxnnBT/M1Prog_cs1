f67f2eda23dec3cefcff68bd2bb98be9aa08a5e8
FxnnBT <40134@ma-web.nl>
2025-09-19 12:29:13 +0200

    variable les commit

---
+++ 00_install_en_project/M1Prog_cs1/02_var/variabeleopdracht1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace variabeleopdracht1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string encryptedmessage = "a9F7xQ2m-P1k8Rz4B-0vW5cE3t-L6n9Yd7";
+        int length = 7;
+        double amount = 3.5;
+        bool iscorrect = true;
+
+        Console.WriteLine(encryptedmessage);
+        Console.WriteLine(length);
+        Console.WriteLine(amount);
+        Console.WriteLine(iscorrect);
+    }
+}
